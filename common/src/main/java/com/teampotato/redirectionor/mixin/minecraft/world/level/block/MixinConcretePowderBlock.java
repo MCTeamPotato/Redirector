493187a2602c8e9b6f43d1177923abc81a8af82c
Kasualix <670067575@qq.com>
2023-07-05 14:17:11 +0800

    fix production crashes

--- common/src/main/java/com/teampotato/redirectionor/mixin/minecraft/world/level/block/MixinConcretePowderBlock.java
+++ common/src/main/java/com/teampotato/redirectionor/mixin/minecraft/world/level/block/MixinConcretePowderBlock.java
@@ -9,13 +9,13 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ConcretePowderBlock.class)
 public abstract class MixinConcretePowderBlock {
-    @Redirect(method = "touchesLiquid", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Direction;values()[Lnet/minecraft/core/Direction;"))
+    /*@Redirect(method = "touchesLiquid", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Direction;values()[Lnet/minecraft/core/Direction;"))
     private static Direction[] implValues() {
         return Redirectionor.DIRECTIONS;
-    }
+    }TODO: Why can't these find targets???
 
     @Redirect(method = "touchesLiquid", at = @At(value = "FIELD", target = "Lnet/minecraft/core/Direction;DOWN:Lnet/minecraft/core/Direction;"))
     private static Direction implDown() {
         return Redirectionor.DOWN;
-    }
+    }*/
 }
