fd9bbb6cec88090467405283b8783ae0f1b058ef
Kasualix <670067575@qq.com>
2023-07-08 10:05:53 +0800

    Release 1182

--- common/src/main/java/com/teampotato/redirectionor/mixin/minecraft/world/level/block/MixinFireBlock.java
+++ common/src/main/java/com/teampotato/redirectionor/mixin/minecraft/world/level/block/MixinFireBlock.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(FireBlock.class)
 public abstract class MixinFireBlock {
-    @Redirect(method = {"getStateForPlacement(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;", "isValidFireLocation", "getIgniteOdds(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;)I"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Direction;values()[Lnet/minecraft/core/Direction;"))
+    @Redirect(method = {"getStateForPlacement(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;", "isValidFireLocation"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Direction;values()[Lnet/minecraft/core/Direction;"))
     private Direction[] implValues() {
         return Redirectionor.DIRECTIONS;
     }
