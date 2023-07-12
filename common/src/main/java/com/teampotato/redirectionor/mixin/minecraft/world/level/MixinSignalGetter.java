02861560ab096dbba11417fe6313c3701bdc574a
Kasualix <670067575@qq.com>
2023-07-12 09:41:05 +0800

    fix https://github.com/MCTeamPotato/Kasualix-Issue-Tracker/issues/6

--- common/src/main/java/com/teampotato/redirectionor/mixin/minecraft/world/level/MixinSignalGetter.java
+++ common/src/main/java/com/teampotato/redirectionor/mixin/minecraft/world/level/MixinSignalGetter.java
@@ -1,41 +1,63 @@
 package com.teampotato.redirectionor.mixin.minecraft.world.level;
 
 import com.teampotato.redirectionor.Redirectionor;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.SignalGetter;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(SignalGetter.class)
 public interface MixinSignalGetter {
-    @Redirect(method = {"getDirectSignalTo", "hasNeighborSignal"}, at = @At(value = "FIELD", target = "Lnet/minecraft/core/Direction;DOWN:Lnet/minecraft/core/Direction;"))
-    private Direction implDown() {
-        return Redirectionor.DOWN;
+    @Shadow int getDirectSignal(BlockPos blockPos, Direction direction);
+
+    @Shadow int getSignal(BlockPos blockPos, Direction direction);
+
+    /**
+     * @author Kasualix
+     * @reason avoid allocation
+     */
+    @Overwrite
+    default boolean hasNeighborSignal(BlockPos blockPos) {
+        if (this.getSignal(blockPos.below(), Redirectionor.DOWN) > 0) {
+            return true;
+        } else if (this.getSignal(blockPos.above(), Redirectionor.UP) > 0) {
+            return true;
+        } else if (this.getSignal(blockPos.north(), Redirectionor.NORTH) > 0) {
+            return true;
+        } else if (this.getSignal(blockPos.south(), Redirectionor.SOUTH) > 0) {
+            return true;
+        } else if (this.getSignal(blockPos.west(), Redirectionor.WEST) > 0) {
+            return true;
+        } else {
+            return this.getSignal(blockPos.east(), Redirectionor.EAST) > 0;
+        }
     }
 
-    @Redirect(method = {"getDirectSignalTo", "hasNeighborSignal"}, at = @At(value = "FIELD", target = "Lnet/minecraft/core/Direction;EAST:Lnet/minecraft/core/Direction;"))
-    private Direction implEast() {
-        return Redirectionor.EAST;
-    }
-
-    @Redirect(method = {"getDirectSignalTo", "hasNeighborSignal"}, at = @At(value = "FIELD", target = "Lnet/minecraft/core/Direction;NORTH:Lnet/minecraft/core/Direction;"))
-    private Direction implNorth() {
-        return Redirectionor.NORTH;
-    }
-
-    @Redirect(method = {"getDirectSignalTo", "hasNeighborSignal"}, at = @At(value = "FIELD", target = "Lnet/minecraft/core/Direction;SOUTH:Lnet/minecraft/core/Direction;"))
-    private Direction implSouth() {
-        return Redirectionor.SOUTH;
-    }
-
-    @Redirect(method = {"getDirectSignalTo", "hasNeighborSignal"}, at = @At(value = "FIELD", target = "Lnet/minecraft/core/Direction;WEST:Lnet/minecraft/core/Direction;"))
-    private Direction implWest() {
-        return Redirectionor.WEST;
-    }
-
-    @Redirect(method = {"getDirectSignalTo", "hasNeighborSignal"}, at = @At(value = "FIELD", target = "Lnet/minecraft/core/Direction;UP:Lnet/minecraft/core/Direction;"))
-    private Direction implUp() {
-        return Redirectionor.UP;
+    /**
+     * @author Kasualix
+     * @reason avoid allocation
+     */
+    @Overwrite
+    default int getDirectSignalTo(BlockPos blockPos) {
+        int i = 0;
+        i = Math.max(i, this.getDirectSignal(blockPos.below(), Redirectionor.DOWN));
+        if (i < 15) {
+            i = Math.max(i, this.getDirectSignal(blockPos.above(), Redirectionor.UP));
+            if (i < 15) {
+                i = Math.max(i, this.getDirectSignal(blockPos.north(), Redirectionor.NORTH));
+                if (i < 15) {
+                    i = Math.max(i, this.getDirectSignal(blockPos.south(), Redirectionor.SOUTH));
+                    if (i < 15) {
+                        i = Math.max(i, this.getDirectSignal(blockPos.west(), Redirectionor.WEST));
+                        if (i < 15) {
+                            i = Math.max(i, this.getDirectSignal(blockPos.east(), Redirectionor.EAST));
+                        }
+                    }
+                }
+            }
+        }
+        return i;
     }
 }
