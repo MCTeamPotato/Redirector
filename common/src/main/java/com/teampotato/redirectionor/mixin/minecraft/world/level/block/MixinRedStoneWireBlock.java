c8db50e0cb38aaf7dc76423cedd56b36712ceaa8
Kasualix <670067575@qq.com>
2023-07-21 09:37:14 +0800

    compatibility with DimThread

--- common/src/main/java/com/teampotato/redirectionor/mixin/minecraft/world/level/block/MixinRedStoneWireBlock.java
+++ common/src/main/java/com/teampotato/redirectionor/mixin/minecraft/world/level/block/MixinRedStoneWireBlock.java
@@ -1,42 +1,21 @@
 package com.teampotato.redirectionor.mixin.minecraft.world.level.block;
 
 import com.teampotato.redirectionor.Redirectionor;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.RedStoneWireBlock;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = RedStoneWireBlock.class, priority = 10)
+@Mixin(value = RedStoneWireBlock.class, priority = 2000)
 public abstract class MixinRedStoneWireBlock {
-    @Shadow protected abstract void updatePowerStrength(Level p_55531_, BlockPos p_55532_, BlockState p_55533_);
-
-    @Shadow protected abstract void updateNeighborsOfNeighboringWires(Level p_55638_, BlockPos p_55639_);
-
-    @Redirect(method = {"updateIndirectNeighbourShapes", "canSurviveOn", "getSignal"}, at = @At(value = "FIELD", target = "Lnet/minecraft/core/Direction;UP:Lnet/minecraft/core/Direction;"))
+    @Redirect(method = {"updateIndirectNeighbourShapes", "canSurviveOn", "getSignal"}, at = @At(value = "FIELD", target = "Lnet/minecraft/core/Direction;UP:Lnet/minecraft/core/Direction;"), require = 0)
     private Direction implUp1() {
         return Redirectionor.UP;
     }
 
-    /**
-     * @author Kasualix
-     * @reason avoid allocation
-     */
-    @Overwrite
-    public void onPlace(BlockState pState, Level pLevel, BlockPos pPos, BlockState pOldState, boolean pIsMoving) {
-        if (!pOldState.is(pState.getBlock()) && !pLevel.isClientSide) {
-            this.updatePowerStrength(pLevel, pPos, pState);
-
-            for(Direction direction : Redirectionor.VERTICAL) {
-                pLevel.updateNeighborsAt(pPos.relative(direction), (RedStoneWireBlock)(Object)this);
-            }
-
-            this.updateNeighborsOfNeighboringWires(pLevel, pPos);
-        }
+    @Redirect(method = "onPlace", at = @At(value = "FIELD", target = "Lnet/minecraft/core/Direction$Plane;VERTICAL:Lnet/minecraft/core/Direction$Plane;"))
+    private Direction.Plane onPlace() {
+        return Redirectionor.VERTICAL;
     }
 }
