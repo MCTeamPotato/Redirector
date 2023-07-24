876cdda6f8dabd6744511fc51c3229b5cc4e9f0c
Kasualix <670067575@qq.com>
2023-07-24 22:07:29 +0800

    println

--- fabric/src/main/java/com/teampotato/redirectionor/fabric/RedirectionorFabric.java
+++ fabric/src/main/java/com/teampotato/redirectionor/fabric/RedirectionorFabric.java
@@ -3,5 +3,7 @@ package com.teampotato.redirectionor.fabric;
 import net.fabricmc.api.ModInitializer;
 
 public class RedirectionorFabric implements ModInitializer {
-    public void onInitialize() {}
+    public void onInitialize() {
+        System.out.println("Redirectionor loaded. It's possible that you have seen some Mixin Apply Failed warning log, but you just don't need care much about them because it's simply override by other optimization mods which hopefully did the same thing.");
+    }
 }
