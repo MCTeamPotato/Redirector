5c9fb9bd195128d84f2df4e218a4c9ddc0471f6f
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2023-08-06 18:36:25 +0800

    Update DensityFunctionsMixin.java

--- common/src/main/java/com/teampotato/redirectionor/mixin/world/level/levelgen/DensityFunctionsMixin.java
+++ common/src/main/java/com/teampotato/redirectionor/mixin/world/level/levelgen/DensityFunctionsMixin.java
@@ -8,18 +8,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(DensityFunctions.class)
 public abstract class DensityFunctionsMixin {
-    @Redirect(method = "bootstrap",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/DensityFunctions$TwoArgumentSimpleFunction$Type;values()[Lnet/minecraft/world/level/levelgen/DensityFunctions$TwoArgumentSimpleFunction$Type;"))
-    private static DensityFunctions.TwoArgumentSimpleFunction.Type[] redirectDensityFunctionsTwoArgumentSimpleFunctionType() {
-        return Redirectionor.DENSITY_FUNCTIONS_TWO_ARGUMENT_SIMPLE_FUNCTION_TYPES;
-    }
-
     @Redirect(method = "bootstrap", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/DensityFunctions$Marker$Type;values()[Lnet/minecraft/world/level/levelgen/DensityFunctions$Marker$Type;"))
     private static DensityFunctions.Marker.Type[] redirectDensityFunctionsMarkerType() {
         return Redirectionor.DENSITY_FUNCTIONS_MARKER_TYPES;
-    }
-
-    @Redirect(method = "bootstrap", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/DensityFunctions$Mapped$Type;values()[Lnet/minecraft/world/level/levelgen/DensityFunctions$Mapped$Type;"))
-    private static DensityFunctions.Mapped.Type[] redirectDensityFunctionsMappedType() {
-        return Redirectionor.DENSITY_FUNCTIONS_MAPPED_TYPES;
     }
 }
