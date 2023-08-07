2db9f7e85c895c007862513b9b3c3179b9c56566
Kasualix <670067575@qq.com>
2023-08-07 12:11:57 +0800

    fix server side crash

--- common/src/main/java/com/teampotato/redirectionor/mixin/client/renderer/block/model/GuiLightMixin.java
+++ common/src/main/java/com/teampotato/redirectionor/mixin/client/renderer/block/model/GuiLightMixin.java
@@ -1,15 +1,17 @@
 package com.teampotato.redirectionor.mixin.client.renderer.block.model;
 
-import com.teampotato.redirectionor.Redirectionor;
 import net.minecraft.client.renderer.block.model.BlockModel;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BlockModel.GuiLight.class)
 public abstract class GuiLightMixin {
+    @Unique
+    private static final BlockModel.GuiLight[] BLOCK_MODEL_GUI_LIGHTS = BlockModel.GuiLight.values();
     @Redirect(method = "getByName", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/model/BlockModel$GuiLight;values()[Lnet/minecraft/client/renderer/block/model/BlockModel$GuiLight;"))
     private static BlockModel.GuiLight[] redirectGuiLight() {
-        return Redirectionor.BLOCK_MODEL_GUI_LIGHTS;
+        return BLOCK_MODEL_GUI_LIGHTS;
     }
 }
