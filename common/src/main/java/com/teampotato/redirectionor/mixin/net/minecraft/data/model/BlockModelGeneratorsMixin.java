0d98caf6d2e9e37dd2f52a9d2c22be2002e15cfe
Kasualix <670067575@qq.com>
2023-08-24 23:49:48 +0800

    Update BlockModelGeneratorsMixin.java

--- common/src/main/java/com/teampotato/redirectionor/mixin/net/minecraft/data/model/BlockModelGeneratorsMixin.java
+++ common/src/main/java/com/teampotato/redirectionor/mixin/net/minecraft/data/model/BlockModelGeneratorsMixin.java
@@ -9,8 +9,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BlockModelGenerators.class)
 public abstract class BlockModelGeneratorsMixin {
-    @Unique
-  private static final DripstoneThickness[] DRIPSTONE_THICKNESSES = DripstoneThickness.values();
+    @Unique private static final DripstoneThickness[] DRIPSTONE_THICKNESSES = DripstoneThickness.values();
     @Redirect(method = "createPointedDripstone", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/properties/DripstoneThickness;values()[Lnet/minecraft/world/level/block/state/properties/DripstoneThickness;"))
     private DripstoneThickness[] redirectDripstoneThickness() {
         return DRIPSTONE_THICKNESSES;
