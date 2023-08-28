6d41da0005c91855c9d586f744851d73e8ffdc99
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2023-08-28 19:33:53 +0800

    try

--- src/main/java/com/Hileb/teampotato.redirectionor/RedirectionorTansformer.java
+++ src/main/java/com/Hileb/teampotato.redirectionor/RedirectionorTansformer.java
@@ -12,37 +12,37 @@ import org.objectweb.asm.tree.*;
  * @Date 2023/8/28 11:52
  **/
 public class RedirectionorTansformer implements IClassTransformer {
-
-
-    boolean isDeBug=false;
-    boolean isFieldUnknown=false;
+    boolean isDeBug=true;
     @Override
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
-        ClassReader classReader=new ClassReader(basicClass);
-        if ("java/lang/Enum".equals(classReader.getSuperName())){
-            ClassNode cn=new ClassNode();
-            classReader.accept(cn,0);
+        try{
+            ClassReader classReader=new ClassReader(basicClass);
+            if ("java/lang/Enum".equals(classReader.getSuperName())){
+                ClassNode cn=new ClassNode();
+                classReader.accept(cn,0);
 
-            String classPath= classReader.getClassName();
-            String typeName="[L"+classPath+";";
+                String classPath= classReader.getClassName();
+                String typeName="[L"+classPath+";";
 
-            for(MethodNode mn:cn.methods){
-                if ("values".equals(mn.name)){
-                    if (mn.desc.contains("()")){
-                        InsnList il=mn.instructions;
-                        il.clear();
-                        il.add(new FieldInsnNode(Opcodes.GETSTATIC,classPath,"$VALUES",typeName));
-                        il.add(new InsnNode(Opcodes.ARETURN));
-                        if (isDeBug)System.out.println("Redirect "+classPath);
-                        break;
+                for(MethodNode mn:cn.methods){
+                    if ("values".equals(mn.name)){
+                        if (mn.desc.contains("()")){
+                            InsnList il=mn.instructions;
+                            il.clear();
+                            il.add(new FieldInsnNode(Opcodes.GETSTATIC,classPath,"$VALUES",typeName));
+                            il.add(new InsnNode(Opcodes.ARETURN));
+                            if (isDeBug)System.out.println("Redirect "+classPath);
+                            break;
+                        }
                     }
                 }
+                ClassWriter classWriter=new ClassWriter(classReader,ClassWriter.COMPUTE_MAXS);
+                cn.accept(classWriter);
+                return classWriter.toByteArray();
             }
-            ClassWriter classWriter=new ClassWriter(classReader,ClassWriter.COMPUTE_MAXS);
-            cn.accept(classWriter);
-            return classWriter.toByteArray();
+        }catch(Exception ignore){
+            return basicClass;
         }
-
         return basicClass;
     }
 }
