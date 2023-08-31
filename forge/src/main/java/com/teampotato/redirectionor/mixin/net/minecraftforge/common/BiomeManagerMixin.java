1d75f17d6ca075abec13b7769de811df8ea81333
Kasualix <670067575@qq.com>
2023-08-31 17:42:27 +0800

    Update BiomeManagerMixin.java

--- forge/src/main/java/com/teampotato/redirectionor/mixin/net/minecraftforge/common/BiomeManagerMixin.java
+++ forge/src/main/java/com/teampotato/redirectionor/mixin/net/minecraftforge/common/BiomeManagerMixin.java
@@ -8,10 +8,11 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = BiomeManager.class, remap = false)
 public abstract class BiomeManagerMixin {
-    @Unique private static final BiomeManager.BiomeType[] BIOME_TYPES = BiomeManager.BiomeType.values();
+    @Unique private static BiomeManager.BiomeType[] BIOME_TYPES = null;
 
     @Redirect(method = "setupBiomes", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/common/BiomeManager$BiomeType;values()[Lnet/minecraftforge/common/BiomeManager$BiomeType;"))
     private static BiomeManager.BiomeType[] redirectBiomeManagerBiomeTypeValues() {
+        if (BIOME_TYPES == null) BIOME_TYPES = BiomeManager.BiomeType.values();
         return BIOME_TYPES;
     }
 }
