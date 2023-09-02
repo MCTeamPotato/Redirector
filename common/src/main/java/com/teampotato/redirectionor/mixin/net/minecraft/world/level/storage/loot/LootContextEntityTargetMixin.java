68769f7e1735a3e81356c02fa8d3b9edae2fa8b7
Kasualix <670067575@qq.com>
2023-09-03 02:50:39 +0800

    try to resolve https://github.com/MCTeamPotato/Kasualix-Issue-Tracker/issues/50

--- common/src/main/java/com/teampotato/redirectionor/mixin/net/minecraft/world/level/storage/loot/LootContextEntityTargetMixin.java
+++ common/src/main/java/com/teampotato/redirectionor/mixin/net/minecraft/world/level/storage/loot/LootContextEntityTargetMixin.java
@@ -16,16 +16,15 @@ public abstract class LootContextEntityTargetMixin {
      */
     @Overwrite
     public static LootContext.EntityTarget getByName(String name) {
+        if (ENTITY_TARGET_NAME_MAP.isEmpty()) {
+            ENTITY_TARGET_NAME_MAP.put("this", LootContext.EntityTarget.THIS);
+            ENTITY_TARGET_NAME_MAP.put("killer", LootContext.EntityTarget.KILLER);
+            ENTITY_TARGET_NAME_MAP.put("direct_killer", LootContext.EntityTarget.DIRECT_KILLER);
+            ENTITY_TARGET_NAME_MAP.put("killer_player", LootContext.EntityTarget.KILLER_PLAYER);
+        }
         return ENTITY_TARGET_NAME_MAP.get(name);
     }
 
     @Unique
     private static final Map<String, LootContext.EntityTarget> ENTITY_TARGET_NAME_MAP = new Object2ObjectOpenHashMap<>();
-
-    static {
-        ENTITY_TARGET_NAME_MAP.put("this", LootContext.EntityTarget.THIS);
-        ENTITY_TARGET_NAME_MAP.put("killer", LootContext.EntityTarget.KILLER);
-        ENTITY_TARGET_NAME_MAP.put("direct_killer", LootContext.EntityTarget.DIRECT_KILLER);
-        ENTITY_TARGET_NAME_MAP.put("killer_player", LootContext.EntityTarget.KILLER_PLAYER);
-    }
 }
