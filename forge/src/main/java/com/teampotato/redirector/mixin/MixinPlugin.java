a4a11a559812f312cabb61023fd8e4313b74c1eb
Kasualix <670067575@qq.com>
2023-09-27 19:27:13 +0800

    fix crash with sculk horde

--- forge/src/main/java/com/teampotato/redirector/mixin/MixinPlugin.java
+++ forge/src/main/java/com/teampotato/redirector/mixin/MixinPlugin.java
@@ -1,5 +1,6 @@
 package com.teampotato.redirector.mixin;
 
+import net.minecraftforge.fml.loading.FMLLoader;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -18,8 +19,10 @@ public class MixinPlugin implements IMixinConfigPlugin {
         return null;
     }
 
+    @SuppressWarnings("RedundantIfStatement")
     @Override
     public boolean shouldApplyMixin(String s, String s1) {
+        if (FMLLoader.getLoadingModList().getModFileById("sculkhorde") != null && s1.contains("FrameTypeMixin")) return false;
         return true;
     }
 
