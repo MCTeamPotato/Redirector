585d75e443d956f312c483a988882397da83e106
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2024-01-27 14:48:26 +0800

    1.3?

--- src/main/java/com/Hileb/teampotato.redirectionor/RedirectionorContainer.java
+++ src/main/java/com/Hileb/teampotato.redirectionor/RedirectionorContainer.java
@@ -1,6 +1,5 @@
 package com.Hileb.teampotato.redirectionor;
 
-import com.google.common.eventbus.EventBus;
 import cpw.mods.fml.common.DummyModContainer;
 import cpw.mods.fml.common.LoadController;
 import cpw.mods.fml.common.ModMetadata;
@@ -17,16 +16,16 @@ public class RedirectionorContainer extends DummyModContainer {
         metadata.modId=Redirectionor.MODID;
         metadata.name="Redirectionor";
         metadata.description="Redirectionor is the implementation of this concept, specifically for the Direction enum class stuff, to reduce the required memory of the game.";
-        metadata.version="1.7.10";
+        metadata.version="1.3 for 1.7.10";
         metadata.url="https://www.curseforge.com/minecraft/mc-mods/redirectionor";
         metadata.logoFile="/icon_redirectionor.png";
         metadata.authorList.add("MCTeamPotato");
         metadata.authorList.add("Kasualix");
         metadata.authorList.add("Hileb");
     }
+
     @Override
-    public boolean registerBus(EventBus bus, LoadController controller) {
-        bus.register(this);
+    public boolean registerBus(com.google.common.eventbus.EventBus bus, LoadController controller) {
         return true;
     }
 }
