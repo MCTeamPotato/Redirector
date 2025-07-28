2930fc33c2e84d17865619ace80434be9831feaf
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2025-07-28 15:36:25 +0800

    make check gen more lazy

--- src/main/java/com/Hileb/teampotato/redirectionor/transformers/RedirectionorTransformer.java
+++ src/main/java/com/Hileb/teampotato/redirectionor/transformers/RedirectionorTransformer.java
@@ -30,14 +30,14 @@ public class RedirectionorTransformer implements IClassTransformer {
                             @Override
                             public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
                                 if ("clone".equals(name)) {
+                                    if (RedirectionorConfig.Config.printTransformedClasses) Redirectionor.LOGGER.info("Redirectionor : " + transformedName);
                                     if (RedirectionorConfig.Config.checkEnumsWhenRunningTime) {
-                                        if (RedirectionorConfig.Config.printTransformedClasses) Redirectionor.LOGGER.info("Redirectionor : " + transformedName);
                                         super.visitMethodInsn(Opcodes.INVOKESTATIC, classReader.getClassName(), "redirectionor_checkEnums", "(" + arrayType + ")" + arrayType, false);
                                     }
                                 } else super.visitMethodInsn(opcode, owner, name, desc, itf);
                             }
                         };
-                    } else if ("<clinit>".equals(name)) {
+                    } else if ("<clinit>".equals(name) && RedirectionorConfig.Config.checkEnumsWhenRunningTime) {
                         return new MethodVisitor(ASM_API, super.visitMethod(access, name, desc, signature, exceptions)) {
 
                             @Override
@@ -54,71 +54,73 @@ public class RedirectionorTransformer implements IClassTransformer {
 
                 @Override
                 public void visitEnd() {
-                    this.visitField(Opcodes.ACC_PRIVATE | Opcodes.ACC_STATIC, "redirectionor_check", "Z", null, null).visitEnd();
-                    MethodVisitor methodVisitor;
-                    {
-                        methodVisitor = this.visitMethod(Opcodes.ACC_PRIVATE | Opcodes.ACC_STATIC, "redirectionor_logError", "()V", null, null);
-                        methodVisitor.visitCode();
-                        methodVisitor.visitTypeInsn(Opcodes.NEW, "java/lang/IllegalStateException");
-                        methodVisitor.visitInsn(Opcodes.DUP);
-                        methodVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/IllegalStateException", "<init>", "()V", false);
-                        methodVisitor.visitMethodInsn(INVOKESTATIC, "com/Hileb/teampotato/redirectionor/Redirectionor", "logError", "(Ljava/lang/Throwable;)V", false);
-                        methodVisitor.visitInsn(Opcodes.RETURN);
-                        methodVisitor.visitMaxs(2, 0);
-                        methodVisitor.visitEnd();
-                    }
-                    {
-                        methodVisitor = this.visitMethod(Opcodes.ACC_PRIVATE | Opcodes.ACC_STATIC, "redirectionor_checkEnums", "(" + arrayType + ")" + arrayType, null, null);
-                        methodVisitor.visitCode();
-                        Label label0 = new Label();
-                        methodVisitor.visitLabel(label0);
-                        methodVisitor.visitFieldInsn(GETSTATIC, classReader.getClassName(), "redirectionor_check", "Z");
-                        Label label1 = new Label();
-                        methodVisitor.visitJumpInsn(IFEQ, label1);
-                        Label label2 = new Label();
-                        methodVisitor.visitLabel(label2);
-                        methodVisitor.visitInsn(ICONST_0);
-                        methodVisitor.visitVarInsn(ISTORE, 1);
-                        Label label3 = new Label();
-                        methodVisitor.visitLabel(label3);
-                        methodVisitor.visitFrame(Opcodes.F_APPEND, 1, new Object[]{Opcodes.INTEGER}, 0, null);
-                        methodVisitor.visitVarInsn(ALOAD, 0);
-                        methodVisitor.visitInsn(ARRAYLENGTH);
-                        methodVisitor.visitVarInsn(ILOAD, 1);
-                        methodVisitor.visitJumpInsn(IF_ICMPLE, label1);
-                        Label label4 = new Label();
-                        methodVisitor.visitLabel(label4);
-                        methodVisitor.visitVarInsn(ALOAD, 0);
-                        methodVisitor.visitVarInsn(ILOAD, 1);
-                        methodVisitor.visitInsn(AALOAD);
-                        methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/Enum", "ordinal", "()I", false);
-                        methodVisitor.visitVarInsn(ILOAD, 1);
-                        Label label5 = new Label();
-                        methodVisitor.visitJumpInsn(IF_ICMPEQ, label5);
-                        Label label6 = new Label();
-                        methodVisitor.visitLabel(label6);
-                        methodVisitor.visitMethodInsn(INVOKESTATIC, classReader.getClassName(), "redirectionor_logError", "()V", false);
-                        Label label7 = new Label();
-                        methodVisitor.visitLabel(label7);
-                        methodVisitor.visitInsn(ICONST_0);
-                        methodVisitor.visitFieldInsn(PUTSTATIC, classReader.getClassName(), "redirectionor_check", "Z");
-                        Label label8 = new Label();
-                        methodVisitor.visitLabel(label8);
-                        methodVisitor.visitJumpInsn(GOTO, label1);
-                        methodVisitor.visitLabel(label5);
-                        methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
-                        methodVisitor.visitIincInsn(1, 1);
-                        methodVisitor.visitJumpInsn(GOTO, label3);
-                        methodVisitor.visitLabel(label1);
-                        methodVisitor.visitFrame(Opcodes.F_CHOP, 1, null, 0, null);
-                        methodVisitor.visitVarInsn(ALOAD, 0);
-                        methodVisitor.visitInsn(ARETURN);
-                        Label label9 = new Label();
-                        methodVisitor.visitLabel(label9);
-                        methodVisitor.visitLocalVariable("i", "I", null, label3, label1, 1);
-                        methodVisitor.visitLocalVariable("enumArray", arrayType, arrayType, label0, label9, 0);
-                        methodVisitor.visitMaxs(2, 2);
-                        methodVisitor.visitEnd();
+                    if (RedirectionorConfig.Config.checkEnumsWhenRunningTime) {
+                        this.visitField(Opcodes.ACC_PRIVATE | Opcodes.ACC_STATIC, "redirectionor_check", "Z", null, null).visitEnd();
+                        MethodVisitor methodVisitor;
+                        {
+                            methodVisitor = this.visitMethod(Opcodes.ACC_PRIVATE | Opcodes.ACC_STATIC, "redirectionor_logError", "()V", null, null);
+                            methodVisitor.visitCode();
+                            methodVisitor.visitTypeInsn(Opcodes.NEW, "java/lang/IllegalStateException");
+                            methodVisitor.visitInsn(Opcodes.DUP);
+                            methodVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/IllegalStateException", "<init>", "()V", false);
+                            methodVisitor.visitMethodInsn(INVOKESTATIC, "com/Hileb/teampotato/redirectionor/Redirectionor", "logError", "(Ljava/lang/Throwable;)V", false);
+                            methodVisitor.visitInsn(Opcodes.RETURN);
+                            methodVisitor.visitMaxs(2, 0);
+                            methodVisitor.visitEnd();
+                        }
+                        {
+                            methodVisitor = this.visitMethod(Opcodes.ACC_PRIVATE | Opcodes.ACC_STATIC, "redirectionor_checkEnums", "(" + arrayType + ")" + arrayType, null, null);
+                            methodVisitor.visitCode();
+                            Label label0 = new Label();
+                            methodVisitor.visitLabel(label0);
+                            methodVisitor.visitFieldInsn(GETSTATIC, classReader.getClassName(), "redirectionor_check", "Z");
+                            Label label1 = new Label();
+                            methodVisitor.visitJumpInsn(IFEQ, label1);
+                            Label label2 = new Label();
+                            methodVisitor.visitLabel(label2);
+                            methodVisitor.visitInsn(ICONST_0);
+                            methodVisitor.visitVarInsn(ISTORE, 1);
+                            Label label3 = new Label();
+                            methodVisitor.visitLabel(label3);
+                            methodVisitor.visitFrame(Opcodes.F_APPEND, 1, new Object[]{Opcodes.INTEGER}, 0, null);
+                            methodVisitor.visitVarInsn(ALOAD, 0);
+                            methodVisitor.visitInsn(ARRAYLENGTH);
+                            methodVisitor.visitVarInsn(ILOAD, 1);
+                            methodVisitor.visitJumpInsn(IF_ICMPLE, label1);
+                            Label label4 = new Label();
+                            methodVisitor.visitLabel(label4);
+                            methodVisitor.visitVarInsn(ALOAD, 0);
+                            methodVisitor.visitVarInsn(ILOAD, 1);
+                            methodVisitor.visitInsn(AALOAD);
+                            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/Enum", "ordinal", "()I", false);
+                            methodVisitor.visitVarInsn(ILOAD, 1);
+                            Label label5 = new Label();
+                            methodVisitor.visitJumpInsn(IF_ICMPEQ, label5);
+                            Label label6 = new Label();
+                            methodVisitor.visitLabel(label6);
+                            methodVisitor.visitMethodInsn(INVOKESTATIC, classReader.getClassName(), "redirectionor_logError", "()V", false);
+                            Label label7 = new Label();
+                            methodVisitor.visitLabel(label7);
+                            methodVisitor.visitInsn(ICONST_0);
+                            methodVisitor.visitFieldInsn(PUTSTATIC, classReader.getClassName(), "redirectionor_check", "Z");
+                            Label label8 = new Label();
+                            methodVisitor.visitLabel(label8);
+                            methodVisitor.visitJumpInsn(GOTO, label1);
+                            methodVisitor.visitLabel(label5);
+                            methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
+                            methodVisitor.visitIincInsn(1, 1);
+                            methodVisitor.visitJumpInsn(GOTO, label3);
+                            methodVisitor.visitLabel(label1);
+                            methodVisitor.visitFrame(Opcodes.F_CHOP, 1, null, 0, null);
+                            methodVisitor.visitVarInsn(ALOAD, 0);
+                            methodVisitor.visitInsn(ARETURN);
+                            Label label9 = new Label();
+                            methodVisitor.visitLabel(label9);
+                            methodVisitor.visitLocalVariable("i", "I", null, label3, label1, 1);
+                            methodVisitor.visitLocalVariable("enumArray", arrayType, arrayType, label0, label9, 0);
+                            methodVisitor.visitMaxs(2, 2);
+                            methodVisitor.visitEnd();
+                        }
                     }
                     super.visitEnd();
                 }
