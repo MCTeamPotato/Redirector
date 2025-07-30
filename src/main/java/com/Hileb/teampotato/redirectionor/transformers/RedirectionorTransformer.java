363fd1354b8045fa6f81a8282702c8c628a65c70
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2025-07-30 12:42:49 +0800

    Update RedirectionorTransformer.java

--- src/main/java/com/Hileb/teampotato/redirectionor/transformers/RedirectionorTransformer.java
+++ src/main/java/com/Hileb/teampotato/redirectionor/transformers/RedirectionorTransformer.java
@@ -62,7 +62,7 @@ public class RedirectionorTransformer implements IClassTransformer {
                             methodVisitor.visitCode();
                             methodVisitor.visitTypeInsn(Opcodes.NEW, "java/lang/IllegalStateException");
                             methodVisitor.visitInsn(Opcodes.DUP);
-                            methodVisitor.visitLdc("Enum#values() has been broken.");
+                            methodVisitor.visitLdcInsn("Enum#values() has been broken.");
                             methodVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/IllegalStateException", "<init>", "(Ljava/lang/String;)V", false);
                             methodVisitor.visitMethodInsn(INVOKESTATIC, "com/Hileb/teampotato/redirectionor/Redirectionor", "logError", "(Ljava/lang/Throwable;)V", false);
                             methodVisitor.visitInsn(Opcodes.RETURN);
